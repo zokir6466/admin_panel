34aa936cd85581f728c36c2f8d6cf363ff501d0e
zokir6466 <73847470+zokir6466@users.noreply.github.com>
2020-11-02 23:41:52 +0500

    Add files via upload

--- setup/database.sql
+++ setup/database.sql
@@ -1,31 +1,15 @@
-create database network;
-create extension pgcrypto;
+create database postgres;
 
-create table clients (
-	id serial not null,
-	username varchar(32) not null,
-	password varchar(60) not null,
-	first_name varchar(32) not null,
-	last_name varchar(32) not null,
-	deleted_at timestamptz,
-	created_at timestamptz default current_timestamp,
-	primary key(id)
+create table users(
+    id bigserial not null,
+    fname varchar(64) not null,
+    lname varchar(64) not null,
+    phone int not null,
+    email varchar(64) not null,
+    primary key (id)
 );
 
-create unique index on clients (lower(username));
-
-insert into clients (
-	username,
-	password,
-	first_name,
-	last_name
-) values
-('sobir',
-	crypt( 'salom', gen_salt('bf')),
-	'Sobir',
-	'Nazarov'),
-	('salim',
-	crypt( 'salom', gen_salt('bf')),
-	'Salim',
-	'Murodov')
-;
+insert into users(fname, lname, phone, email) values('Zokir', 'Mahamadjonov', 974026466, 'zmaxamadjonov29@gmail.com' );
+insert into users(fname, lname, phone, email) values('Bobur', 'Nizomov', 981620516, 'nbobur@gmail.com' );
+insert into users(fname, lname, phone, email) values('Muhammad', 'Najimov', 998961000, 'najimovmuhammad@gmail.com' );
+insert into users(fname, lname, phone, email) values('Nafisa', 'Umrova', 998642015, 'nafisa2015@gmail.com' );
